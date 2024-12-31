4a435fa53823c08a97ac62c21c9b80ba14f43d1f
mocos <130593875+MoXcz@users.noreply.github.com>
2024-12-31 16:26:00 -0500

    Add extra tests for html node
    There wasn't a test for print result of the class and its values.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -12,7 +12,7 @@ class TestHTMLNode(unittest.TestCase):
     def test_not_eq(self):
         node = HTMLNode("p", "this is a paragraph", None, {"class": "paragraph"})
         node2 = HTMLNode("h1", "this is a header", None, {"class": "header"})
-        self.assertNotEqual(node, node2)
+        self.assertNotEqual(node.value, node2.value)
 
     def test_props(self):
         node = HTMLNode(
@@ -26,6 +26,17 @@ class TestHTMLNode(unittest.TestCase):
         )
         self.assertEqual(
             node.props_to_html(), ' href="https://www.google.com" target="_blank"'
+        )
+
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "lazy"},
+        )
+        self.assertEqual(
+            "HTMLNode(p, What a strange world, None, {'class': 'lazy'})", repr(node)
         )
 
 
