28ea5292aacae0a32799bc57671881583d2ee603
mocos <130593875+MoXcz@users.noreply.github.com>
2024-12-30 21:58:12 -0500

    Add TextNode class to represent inline elements

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    text = TextNode("This is some bold text", "bold", "https://www.boldtext.com")
+    print(text)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    NORMAL_TEXT = "normal"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = TextType(text_type)
+        self.url = url
+
+    def __eq__(self, node):
+        if self.text != node.text:
+            return False
+        if self.text_type != node.text_type:
+            return False
+        if self.url != node.url:
+            return False
+        return True
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
