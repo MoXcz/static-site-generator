9f733fd9dbe2373b2a47706a0f89d362027b58fa
mocos <130593875+MoXcz@users.noreply.github.com>
2025-01-01 18:37:32 -0500

    Add function to convert a text node into a HTML node
    The node created is without children, as the nested property is added
    after this conversion.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -17,7 +17,7 @@ class TestTextNode(unittest.TestCase):
     def test_url(self):
         node = TextNode("This is a link node", TextType.LINK, "https://g.com")
         node2 = TextNode("This is an image node", TextType.IMAGE, "http://g.com")
-        self.assertNotEqual(node, node2)
+        self.assertNotEqual(node.url, node2.url)
 
     def test_url_none(self):
         node = TextNode("This is a link node", TextType.LINK)
@@ -29,6 +29,30 @@ class TestTextNode(unittest.TestCase):
         )
         self.assertEqual(
             "TextNode(This is a text node, normal, https://www.boot.dev)", repr(node)
+        )
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.NORMAL_TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_code(self):
+        node = TextNode("print('hi')", TextType.CODE_TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "print('hi')")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
         )
 
 
