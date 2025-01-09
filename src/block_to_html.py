f0aa089c9cbf43ba2f786c5d6eec00cd5f96e195
mocos <130593875+MoXcz@users.noreply.github.com>
2025-01-08 21:42:25 -0500

    Add comments and structure to block_to_html

--- src/block_to_html.py
+++ src/block_to_html.py
@@ -6,6 +6,35 @@ from htmlnode import ParentNode
 from textnode import text_node_to_html_node, text_to_textnodes
 
 
+# Decided to use an Enum to structure the different "types" an HTML block could have
+# so that the "block_node_to_html_node()" function could accept a class structure
+class BlockType(Enum):
+    QUOTE = "quote"
+    UL = "unordered list"
+    OL = "ordered list"
+    CODE = "code"
+    H = "heading"
+    P = "paragraph"
+
+
+# The class used in the aforementioned function
+class BlockNode:
+    def __init__(self, text, block_type):
+        self.text = text
+        self.block_type = BlockType(block_type)
+
+    # Used for Debugging
+    def __eq__(self, node):
+        if self.text != node.text:
+            return False
+        if self.block_type != node.block_type:
+            return False
+        return True
+
+    def __repr__(self):
+        return f"BlockNode({self.text}, {self.block_type.value})"
+
+
 def markdown_to_html_node(markdown):
     children = []
     markdown_blocks = markdown_to_blocks(markdown)
@@ -27,31 +56,9 @@ def markdown_to_html_node(markdown):
     return ParentNode("div", children, None)
 
 
-class BlockType(Enum):
-    QUOTE = "quote"
-    UL = "unordered list"
-    OL = "ordered list"
-    CODE = "code"
-    H = "heading"
-    P = "paragraph"
-
-
-class BlockNode:
-    def __init__(self, text, block_type):
-        self.text = text
-        self.block_type = BlockType(block_type)
-
-    def __eq__(self, node):
-        if self.text != node.text:
-            return False
-        if self.block_type != node.block_type:
-            return False
-        return True
-
-    def __repr__(self):
-        return f"BLockNode({self.text}, {self.block_type.value})"
-
-
+# This class receives a "BlockNode' instance with some text and type to "match"
+# the different cases and parse the "text" field into its assigned "block_type"
+# field of HTML (returned as a "ParentNode" instance
 def block_node_to_html_node(block_node):
     match block_node.block_type:
         case BlockType.QUOTE:
@@ -63,6 +70,7 @@ def block_node_to_html_node(block_node):
             line_blocks = block_node.text.split("\n")
             list_items = []
             for item in line_blocks:
+                # First get all list items
                 list_items.append(
                     ParentNode(
                         "li",
@@ -71,11 +79,13 @@ def block_node_to_html_node(block_node):
                         ),
                     )
                 )
+            # And then add them as children of the parent element
             return ParentNode("ul", list_items, None)
         case BlockType.OL:
             line_blocks = block_node.text.split("\n")
             list_items = []
             for item in line_blocks:
+                # First get all list items
                 list_items.append(
                     ParentNode(
                         "li",
@@ -84,6 +94,7 @@ def block_node_to_html_node(block_node):
                         ),
                     )
                 )
+            # And then add them as children of the parent element
             return ParentNode("ol", list_items, None)
         case BlockType.CODE:
             code_node = ParentNode(
@@ -91,31 +102,40 @@ def block_node_to_html_node(block_node):
             )
             return ParentNode("pre", None, [code_node])
         case BlockType.H:
+            # Match any hashes #
             match = re.match(r"^(#+) ", block_node.text)
             if match:
                 num_hashes = len(match.group(1))
-                text = block_node.text[num_hashes + 1 :]
+                # Remove all hashes by returning the string without them
+                text = block_node.text[num_hashes + 1 :]  # +1 because of " "
                 return ParentNode(f"h{num_hashes}", text_to_children(text))
         case BlockType.P:
             return ParentNode(
-                "p", text_to_children(" ".join(block_node.text.split("\n")))
+                "p",
+                # Remove new lines from text
+                text_to_children(" ".join(block_node.text.split("\n"))),
             )
 
 
+# In the beginning was going to be used to remove all Markdown syntax from the
+# text, but ended up being quite cumbersome. Decided not to refactor it to leave
+# traces of my thought process
 def markdown_block_node_to_text(markdown, block_type):
     match block_type:
         case "code":
             return markdown.replace("```", "")
         case "quote":
-            return markdown.replace("> ", "")
+            return markdown[2:]  # Remove "> " from text
         case "unordered_list_item":
-            return markdown[2:]
+            return markdown[2:]  # Remove any "* " and "- " from text
         case "ordered_list_item":
-            return markdown[3:]
-        case _:
+            return markdown[3:]  # Remove any index "n. " where "n" is a number
+        case _:  # In case it's used in a paragraph it return the same text
             return markdown
 
 
+# This helper function is used to convert Markdown quote lines (">") into a
+# single string of text so that it can be used in "text_to_children()"
 def process_blockquote_text(text):
     lines = text.split("\n")
     new_lines = []
@@ -125,6 +145,10 @@ def process_blockquote_text(text):
     return " ".join(new_lines)
 
 
+# This helper function is used to convert text (without Markdown characters)
+# into instances of "LeafNode"s (which are inline HTML elements)
+# This facilitates having to write a specific conversion for each "match" by
+# receiving only the text
 def text_to_children(text):
     text_nodes = text_to_textnodes(text)
     children = []
