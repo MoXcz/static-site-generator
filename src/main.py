599ca660d245b1ebd2232b8186331b6bc56773dd
mocos <130593875+MoXcz@users.noreply.github.com>
2025-01-09 18:22:22 -0500

    Update main to reflect change with static files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-from textnode import TextNode
+from copy_static_files import src_to_dir
 
 
 def main():
-    text = TextNode("This is some bold text", "bold", "https://www.boldtext.com")
-    print(text)
+    dir_public = "public/"  # delete this one
+    dir_static = "static/"  # copy to this one
+    src_to_dir(dir_static, dir_public)
 
 
 if __name__ == "__main__":
