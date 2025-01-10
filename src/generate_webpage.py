935e0660fe132cd7a8b6c41d46baf785c4aa3594
mocos <130593875+MoXcz@users.noreply.github.com>
2025-01-10 01:47:44 -0500

    Add recursive behavior to page generation
    This is done to generate the according HTML file with nested Markdown
    files.

--- src/generate_webpage.py
+++ src/generate_webpage.py
@@ -1,4 +1,4 @@
-from os import makedirs, path
+from os import listdir, makedirs, path
 
 from block_markdown import markdown_to_blocks
 from block_to_html import markdown_to_html_node, process_heading_text
@@ -26,3 +26,15 @@ def genereate_page(from_path, template_path, dest_path):
         if not path.exists(dest_path):
             makedirs(path.dirname(dest_path), exist_ok=True)
             open(dest_path, "x").write(template_file)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for dir in listdir(dir_path_content):
+        content_dir = path.join(dir_path_content, dir)
+        dest_dir = path.join(dest_dir_path, dir).replace(".md", ".html")
+        if path.isfile(content_dir):
+            genereate_page(content_dir, template_path, dest_dir)
+        else:
+            if not path.exists(dest_dir):
+                makedirs(dest_dir)
+            generate_pages_recursive(content_dir, template_path, dest_dir)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 from copy_static_files import src_to_dir
-from generate_webpage import genereate_page
+from generate_webpage import generate_pages_recursive
 
 
 def main():
     dir_public = "public/"  # delete this one
     dir_static = "static/"  # copy to this one
     src_to_dir(dir_static, dir_public)
-    markdown_file = "content/index.md"
+    content_path = "content/"
     template_file = "template.html"
-    destionation_path = "public/index.html"
-    genereate_page(markdown_file, template_file, destionation_path)
+    public_path = "public/"
+    generate_pages_recursive(content_path, template_file, public_path)
 
 
 if __name__ == "__main__":
