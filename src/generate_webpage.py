4eee6d3e326105df635ee3d305d9a927a02ca324
mocos <130593875+MoXcz@users.noreply.github.com>
2025-03-02 19:04:32 -0500

    GitHub Pages

--- src/generate_webpage.py
+++ src/generate_webpage.py
@@ -13,28 +13,31 @@ def extract_title(markdown):
     raise ValueError(f"No valid h1 header in: {markdown}")
 
 
-def genereate_page(from_path, template_path, dest_path):
+def genereate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     if path.exists(from_path):
         markdown_file = open(from_path).read()
         template_file = open(template_path).read()
         html_content = markdown_to_html_node(markdown_file).to_html()
         title = extract_title(markdown_file)
-        template_file = template_file.replace("{{ Title }}", title).replace(
-            "{{ Content }}", html_content
+        template_file = (
+            template_file.replace("{{ Title }}", title)
+            .replace("{{ Content }}", html_content)
+            .replace('href="/', 'href="' + basepath)
+            .replace('src="/', 'src="' + basepath)
         )
         if not path.exists(dest_path):
             makedirs(path.dirname(dest_path), exist_ok=True)
             open(dest_path, "x").write(template_file)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for dir in listdir(dir_path_content):
         content_dir = path.join(dir_path_content, dir)
         dest_dir = path.join(dest_dir_path, dir).replace(".md", ".html")
         if path.isfile(content_dir):
-            genereate_page(content_dir, template_path, dest_dir)
+            genereate_page(content_dir, template_path, dest_dir, basepath)
         else:
             if not path.exists(dest_dir):
                 makedirs(dest_dir)
-            generate_pages_recursive(content_dir, template_path, dest_dir)
+            generate_pages_recursive(content_dir, template_path, dest_dir, basepath)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
 from copy_static_files import src_to_dir
 from generate_webpage import generate_pages_recursive
+import sys
 
 
 def main():
-    dir_public = "public/"  # delete this one
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    dir_public = "docs/"  # delete this one
     dir_static = "static/"  # copy to this one
     src_to_dir(dir_static, dir_public)
     content_path = "content/"
     template_file = "template.html"
-    public_path = "public/"
-    generate_pages_recursive(content_path, template_file, public_path)
+    public_path = "docs/"
+    generate_pages_recursive(content_path, template_file, public_path, basepath)
 
 
 if __name__ == "__main__":
